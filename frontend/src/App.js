4c1eef81c724281ad477c276e39ff8b4996a15e6
Anudeep <anudeep.pandiri@gmail.com>
2018-09-12 18:53:45 -0500

    Added Social login buttons.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TwitterLogin from 'react-twitter-auth';
+import FacebookLogin from 'react-facebook-login';
+import { GoogleLogin } from 'react-google-login';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+    constructor() {
+        super();
+        this.state = { isAuthenticated: false, user: null, token: ''};
+    }
+
+    logout = () => {
+        this.setState({isAuthenticated: false, token: '', user: null})
+    };
+
+    twitterResponse = (e) => {};
+
+    facebookResponse = (e) => {};
+
+    googleResponse = (e) => {};
+    onFailure = (error) => {
+        alert(error);
+    }
+    render() {
+        let content = !!this.state.isAuthenticated ?
+            (
+            <div>
+            <p>Authenticated</p>
+            <div>
+            {this.state.user.email}
+    </div>
+        <div>
+        <button onClick={this.logout} className="button">
+            Log out
+        </button>
+        </div>
+        </div>
+    ) :
+        (
+        <div>
+        <TwitterLogin loginUrl="http://localhost:4000/api/v1/auth/twitter"
+        onFailure={this.twitterResponse} onSuccess={this.twitterResponse}
+        requestTokenUrl="http://localhost:4000/api/v1/auth/twitter/reverse"/>
+            <FacebookLogin
+        appId="XXXXXXXXXX"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={this.facebookResponse} />
+        <GoogleLogin
+        clientId="XXXXXXXXXX"
+        buttonText="Login"
+        onSuccess={this.googleResponse}
+        onFailure={this.googleResponse}
+        />
+        </div>
     );
-  }
+
+        return (
+            <div className="App">
+            {content}
+            </div>
+    );
+    }
 }
 
 export default App;
